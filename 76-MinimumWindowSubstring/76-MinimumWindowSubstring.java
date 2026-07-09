20ea8bd243c1542b1c040dcc0e67b91201b8930f
swathi <swathipalani30@gmail.com>
2026-07-09 09:02:20 +0530

    Create 76-MinimumWindowSubstring

---
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
@@ -0,0 +1,44 @@
+// Last updated: 7/9/2026, 9:02:20 AM
+import java.util.*;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) return "";
+
+        Map<Character, Integer> need = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int have = 0, needCount = need.size();
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
+                have++;
+            }
+
+            while (have == needCount) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
+                    have--;
+                }
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
