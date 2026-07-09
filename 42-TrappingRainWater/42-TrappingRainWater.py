878bbfc2eef400cbfbd02748925723b4ee138b2c
swathi <swathipalani30@gmail.com>
2026-07-09 09:03:18 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,22 @@
+# Last updated: 7/9/2026, 9:03:18 AM
+class Solution:
+    def trap(self, height):
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    water += right_max - height[right]
+                right -= 1
+
+        return water
