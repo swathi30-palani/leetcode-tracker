3416e775a915ef0a33f0bcb8bcefc39a9ad94eab
swathi <swathipalani30@gmail.com>
2026-07-09 09:03:57 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,24 @@
+# Last updated: 7/9/2026, 9:03:56 AM
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        fast = slow = dummy
+
+        # Move fast pointer n+1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both pointers until fast reaches the end
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        # Remove the nth node from end
+        slow.next = slow.next.next
+
+        return dummy.next
