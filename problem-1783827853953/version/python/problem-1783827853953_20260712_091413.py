58abf9227fee3379d8ec3fcf89125ccc2ec7115a
swathi <swathipalani30@gmail.com>
2026-07-12 09:14:14 +0530

    Create

---
+++ problem-1783827853953/version/python/problem-1783827853953_20260712_091413.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2026, 9:14:13 AM
+1class Solution(object):
+2    def secondsBetweenTimes(self, startTime, endTime):
+3        def to_seconds(time):
+4            h, m, s = map(int, time.split(":"))
+5            return h * 3600 + m * 60 + s
+6        start = to_seconds(startTime)
+7        end = to_seconds(endTime)
+8
+9        if end >= start:
+10            return end - start
+11        else:
+12            return(24 * 3600 - start) + end
+13        
